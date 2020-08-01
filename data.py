0f43773154eb43a8fcaed7c9e721607b9a8380ba
coding-fantastic <alexnzioka@protonmail.com>
2020-08-01 13:34:19 +0300

    first commit

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'lorem lorem',
+            'author': 'Alex Nzioka',
+            'create_date': '04-05-2020'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'lorem lorem',
+            'author': 'Alex Nzioka',
+            'create_date': '04-05-2020'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'lorem lorem',
+            'author': 'Alex Nzioka',
+            'create_date': '04-05-2020'
+        },
+    ]
+    return articles
